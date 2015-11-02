da47106164bc4416da0acd0d98e19049d2301251
Talia <talicohen88@gmail.com>
2015-11-02 12:37:41 +0200

    try to revert

--- etgarPlus/Pages/AddProduct.aspx.cs
+++ etgarPlus/Pages/AddProduct.aspx.cs
@@ -104,7 +104,43 @@ namespace etgarPlus.Pages
             string fileName = "";
             string SaveLocation = "";
 
+<<<<<<< HEAD
             
+=======
+            if ((File1.PostedFile != null) && (File1.PostedFile.ContentLength > 0))
+            {
+                fn = System.IO.Path.GetFileName(File1.PostedFile.FileName);
+                SaveLocation = Server.MapPath("~/images") + "\\" + fn;
+                try
+                {
+                    File1.PostedFile.SaveAs(SaveLocation);
+                    Response.Write("The file has been uploaded.");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error: " + ex.Message);
+                    //Note: Exception.Message returns a detailed message that describes the current exception. 
+                    //For security reasons, we do not recommend that you return Exception.Message to end users in 
+                    //production environments. It would be better to put a generic error message. 
+                }
+            }
+            else
+            {
+                Response.Write("Please select a file to upload.");
+            }
+
+
+
+
+
+
+
+            //FileUpload1.PostedFile.SaveAs(Server.MapPath("~/images/" + fileName));
+            // Session["image"] = "~/images/" + fileName;
+            // img1.ImageUrl = "~/images/" + fileName;
+            //Response.Redirect("AddProduct.aspx");
+            // Debug.WriteLine(RetailPrice.Value);
+>>>>>>> parent of 343b58c... try_4 saved images
 
             if (RetailPrice.Value.ToString().Equals(""))
             {
